c8a89b2c399afa7cd4bd19025af7cdfbe9c11f04
masoud8840 <masoudgharedaghi@yahoo.com>
2022-02-17 15:55:00 +0330

    working on contact page

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,16 @@
       <div class="dropdown">
         <button class="drop-down-btn">حساب کاربری</button>
         <ul class="drop-down-menu">
-          <i class="fa-solid fa-sort-up arrow-up"></i>
+          <div class="arrow-up"></div>
           <li>
             <router-link to="/"><i class="fa-solid fa-user-plus"></i> ثبت نام</router-link>
           </li>
           <li>
             <router-link to="/"><i class="fa-solid fa-door-open"></i> ورود</router-link>
+          </li>
+          <hr>
+          <li>
+            <router-link to="/cart"><i class="fa-solid fa-cart-shopping"></i> سبد خرید </router-link>
           </li>
         </ul>
       </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../views/Home.vue";
+import ContactUs from "../views/ContactUs.vue";
+
 const routes = [
   {
     path: "/",
     component: Home,
   },
+  {
+    path: "/contact",
+    component: ContactUs
+  }
 ];
 
 const router = createRouter({
